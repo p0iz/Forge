21599f5b37939ca398780dd1e2f0339b0314ad6a
Tommi Martela <tommi.martela@gmail.com>
2012-11-14 16:29:15 +0200

    Make scene drawing virtual

--- src/Forge/Graphics/RendererWidget.h
+++ src/Forge/Graphics/RendererWidget.h
@@ -1,7 +1,7 @@
 /* This file is part of Forge.
  *
  * Forge is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
+ * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
  *
@@ -12,8 +12,8 @@
  *
  * You should have received a copy of the GNU Lesser General
  * Public License along with Forge.  If not, see
- * <http://www.gnu.org/licenses/>. 
- * 
+ * <http://www.gnu.org/licenses/>.
+ *
  * Copyright 2012 Tommi Martela
  *
  */
@@ -38,16 +38,16 @@ class RendererWidget : public QGLWidget
 	Q_OBJECT
 public:
 	explicit RendererWidget(
-            Camera& camera,
+			Camera& camera,
 			QtInputHandler& input,
 			QWidget* parent=0,
 			const QGLWidget* shareWidget = 0,
-            Qt::WindowFlags f=0);
+			Qt::WindowFlags f=0);
 
-    void setCamera(Camera& camera)
-    {
-        mCamera = camera;
-    }
+	void setCamera(Camera& camera)
+	{
+		mCamera = camera;
+	}
 
 	virtual ~RendererWidget();
 
@@ -58,7 +58,7 @@ protected:
 	virtual void resizeGL(int w, int h);
 	virtual void paintGL();
 	virtual void keyPressEvent(QKeyEvent* event);
-    virtual void keyReleaseEvent(QKeyEvent* event);
+	virtual void keyReleaseEvent(QKeyEvent* event);
 	virtual void mousePressEvent(QMouseEvent *event);
 	virtual void mouseMoveEvent(QMouseEvent *event);
 private:
@@ -69,10 +69,11 @@ private:
 
 	TestData mTestData;
 
-	void setupScene();
-	void tearDownScene();
+	virtual void setupScene();
+	virtual void drawScene();
+	virtual void tearDownScene();
 
-    Camera& mCamera;
+	Camera& mCamera;
 	QtInputHandler& mInput;
 
 	glm::mat4x4 mProjectionTx;
