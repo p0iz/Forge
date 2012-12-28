54857a349d886dfe5863c9f7d2a0586959469e0e
Tommi Martela <tommi.martela@gmail.com>
2012-12-28 22:26:02 +0000

    Cosmetic changes

--- src/Forge/Graphics/Vertex.h
+++ src/Forge/Graphics/Vertex.h
@@ -1,7 +1,7 @@
 /* This file is part of Forge.
  *
  * Forge is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
+ * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
  *
@@ -12,8 +12,8 @@
  *
  * You should have received a copy of the GNU Lesser General
  * Public License along with Forge.  If not, see
- * <http://www.gnu.org/licenses/>. 
- * 
+ * <http://www.gnu.org/licenses/>.
+ *
  * Copyright 2012 Tommi Martela
  *
  */
@@ -27,21 +27,23 @@ namespace Forge {
 
 struct Vertex
 {
-    Vertex(const glm::vec3& p, const glm::vec2& u, const glm::vec3& n)
-    {
-        position[0] = p[0];
-        position[1] = p[1];
-        position[2] = p[2];
-        uv[0] = u[0];
-        uv[1] = u[1];
-        normal[0] = n[0];
-        normal[1] = n[1];
-        normal[2] = n[2];
-    }
+	Vertex(const glm::vec3& p, const glm::vec2& u, const glm::vec3& n)
+	{
+		position[0] = p[0];
+		position[1] = p[1];
+		position[2] = p[2];
+		uv[0] = u[0];
+		uv[1] = u[1];
+		normal[0] = n[0];
+		normal[1] = n[1];
+		normal[2] = n[2];
+	}
 
-    glm::float_t position[3];
-    glm::float_t uv[2];
-    glm::float_t normal[3];
+	glm::float_t position[3];
+	glm::float_t uv[2];
+	glm::float_t normal[3];
+	glm::float_t tangent[3];
+	glm::float_t bitangent[3];
 };
 
 } // namespace Forge
