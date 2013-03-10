b12754370213c0088978035910f89428c72ad90f
Tommi Martela <tommi.martela@gmail.com>
2013-03-10 23:35:06 +0000

    Fix color selection for UnshadedColor technique.
    The dynamic property should be set for each mesh
    since it changes per-mesh.

--- src/Forge/Graphics/Material/Technique/UnshadedColor.cpp
+++ src/Forge/Graphics/Material/Technique/UnshadedColor.cpp
@@ -71,9 +71,6 @@ void UnshadedColor::updateProperties(const JsonObject &)
 void UnshadedColor::beginMaterial(const RenderTask& task)
 {
 	shaderProgram.use();
-	if (hasProperty(HashUtils::StringHash("Color"))) {
-		glUniform3fv(colorLocation, 1, getProperty(HashUtils::StringHash("Color")));
-	}
 }
 
 void UnshadedColor::beginMesh(const RenderTask& task)
@@ -82,6 +79,9 @@ void UnshadedColor::beginMesh(const RenderTask& task)
 	const Camera& camera = task.getCamera();
 	const glm::mat4x4& vp = camera.getViewProjectionMatrix();
 	glUniformMatrix4fv(wvpLocation, 1, GL_FALSE, &(vp * task.getWorldTransform())[0][0]);
+	if (hasProperty(HashUtils::StringHash("Color"))) {
+		glUniform3fv(colorLocation, 1, getProperty(HashUtils::StringHash("Color")));
+	}
 }
 
 }
