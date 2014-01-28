ed05cbff9e59f931c99c2441599da2110d4f9d87
Tommi Martela <tommi.martela@gmail.com>
2014-01-28 21:42:56 +0200

    Add Lua test script

---
+++ test.lua
@@ -0,0 +1,92 @@
+function print_table(table)
+  for k,v in pairs(Assets)
+  do
+    print(k, v)
+    if type(v) == 'table' then
+      print_table(v)
+    end
+  end
+end
+
+Assets.setLoaderPath 'src/Forge/AssetPlugins'
+Assets.addLoader 'ObjLoader'
+Assets.addLoader 'ImageLoader'
+mesh = Assets.load 'data/meshes/paddle.obj'
+
+-- Create test mesh and scene node
+Scene.createNode 'test'
+Scene.attach(Scene.nodes.test, mesh)
+
+-- Create camera and camera node
+Scene.createCamera('main', 45, 1, 100)
+Scene.createNode 'camera'
+Scene.translate(Scene.nodes.camera, 0, 5, -10)
+Scene.attach(Scene.nodes.camera, Scene.cameras.main)
+
+function print_table(table, indent)
+  local indentation = indent or 0
+  for k,v in pairs(table)
+  do
+    if indentation > 0 then
+      for i=1,indentation do
+        io.write('\t')
+      end
+    end
+    print(k, v)
+    if type(v) == 'table' then
+      print_table(v, indentation + 1)
+    end
+  end
+end
+
+Renderer.createViewport('top', 0, 0.5, 1, 0.5)
+Renderer.createViewport('bottomLeft', 0, 0, 0.5, 0.5)
+Renderer.createViewport('bottomRight', 0.5, 0, 0.5, 0.5)
+
+Renderer.bindCamera('top', Scene.cameras.main)
+Renderer.start()
+
+print('\nAsset table contents:\n')
+print_table(Assets)
+
+print('\n Scene table contents:\n')
+print_table(Scene)
+
+print('\n Renderer table contents:\n')
+print_table(Renderer)
+
+start = os.clock()
+current = os.clock()
+while current - start < 5.0
+do
+  current = os.clock()
+end
+
+--[[ 
+
+-- Acquire mesh assets and camera for rendering
+
+-- Should be able to render meshes now!
+
+]]--
+
+--[[
+
+Need the camera in renderer.
+
+1. get camera properties (perspective matrix)
+2. get camera node transform (view matrix) 
+
+For each mesh
+  3. Get mesh transform (world matrix)
+  4. Get mesh material
+  5. Apply matrices to material uniforms
+  6. Draw mesh using material
+
+RendererThread needs to know about assets and scene data
+
+AssetsLibrary must provide C++ with asset data
+
+SceneLibrary must provide C++ with scene data
+
+]]--
