58fab1567415a972bad06c3e4f8db7fabd8fa555
Tommi Martela <tommi.martela@gmail.com>
2015-08-02 23:57:43 +0300

    Move dependencies inside source tree
    Mainly to simplify Windows development.

--- ForgeWin32Variables.cmake
+++ ForgeWin32Variables.cmake
@@ -2,10 +2,10 @@
 # dependency locations in Windows and other
 # possibly other non-standardized platforms
 
-set(FREEIMAGE_DIR "C:/Development/freeimage")
-set(FT2_DIR "C:/Development/freetype")
-set(GLEW_DIR "C:/Development/glew-1.10.0")
-set(GLM_DIR "C:/Development/glm")
-set(LUA_DIR "C:/Development/lua-5.2.2")
-set(SDL2_DIR "C:/Development/SDL2-2.0.1")
+set(FREEIMAGE_DIR "${CMAKE_SOURCE_DIR}/deps/freeimage")
+set(FT2_DIR "${CMAKE_SOURCE_DIR}/deps/freetype")
+set(GLEW_DIR "${CMAKE_SOURCE_DIR}/deps/glew")
+set(GLM_DIR "${CMAKE_SOURCE_DIR}/deps/glm")
+set(LUA_DIR "${CMAKE_SOURCE_DIR}/deps/luajit")
+set(SDL2_DIR "${CMAKE_SOURCE_DIR}/deps/sdl2")
 
